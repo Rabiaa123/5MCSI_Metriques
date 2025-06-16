c36890a8f002fb94c9ece96928569134ea41849f
Rabiaa123 <r.amarire@gmail.com>
2025-06-16 11:25:29 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,6 +34,10 @@ def mongraphique():
 @app.route("/histogramme/")
 def mon_histogramme():
     return render_template("histogramme.html")
+
+@app.route("/contact/")
+def contact_form():
+    return render_template("contact.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
