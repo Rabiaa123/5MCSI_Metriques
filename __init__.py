a2525863c1b6f0f6f90e577772a3bcf9615ee6b1
Rabiaa123 <r.amarire@gmail.com>
2025-06-16 11:30:47 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template_string, render_template, jsonify
 from flask import render_template
 from flask import json
 from datetime import datetime
+from collections import Counter
 from urllib.request import urlopen
 import sqlite3
 
@@ -34,6 +35,27 @@ def mon_histogramme():
 @app.route("/contact/")
 def contact_form():
     return render_template("contact.html")
+
+@app.route('/commits/')
+def commits():
+    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
+    response = urlopen(url)
+    raw_data = response.read()
+    json_data = json.loads(raw_data.decode('utf-8'))
+
+    minutes_list = []
+    for commit in json_data:
+        date_str = commit.get('commit', {}).get('author', {}).get('date')
+        if date_str:
+            date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
+            minutes_list.append(date_obj.minute)
+
+    # Compter le nombre de commits par minute
+    count_per_minute = dict(Counter(minutes_list))
+    results = [{'minute': minute, 'commits': count} for minute, count in sorted(count_per_minute.items())]
+
+    return jsonify(results=results)
+
   
 if __name__ == "__main__":
   app.run(debug=True)
